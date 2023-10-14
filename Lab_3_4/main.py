9234600e75115d5ffb0f5f5fd5272041a2399bbd
Solt01 <70744585+Solt01@users.noreply.github.com>
2023-10-14 14:15:38 +0300

    Add files via upload

--- Lab_3_4/main.py
+++ Lab_3_4/main.py
@@ -1,18 +1,22 @@
 import multiprocessing, psutil, time
 from hashlib import sha256
+from typing import Final
+
+START: Final[int] = 97
+END: Final[int] = 97+26
 
 h1 = bytes().fromhex("1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad")
 h2 = bytes().fromhex("3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b")
 h3 = bytes().fromhex("74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f")
 
 def brute(firstletterascii: int):
-    global h1, h2, h3
+    global h1, h2, h3, START, END
     letters = bytearray(5)
     letters[0] = firstletterascii
-    for letters[1] in range(97, 97 + 26):
-        for letters[2] in range(97, 97 + 26):
-            for letters[3] in range(97, 97 + 26):
-                for letters[4] in range(97, 97 + 26):
+    for letters[1] in range(START, END):
+        for letters[2] in range(START, END):
+            for letters[3] in range(START, END):
+                for letters[4] in range(START, END):
                     digest = sha256(letters).digest()
                     if digest == h1 or digest == h2 or digest == h3:
                         password = "".join(chr(x) for x in letters)
@@ -21,7 +25,7 @@ def brute(firstletterascii: int):
 
 def main(thread=psutil.cpu_count()):
     with multiprocessing.Pool(processes=thread) as p:
-        p.map(brute, range(97, 97 + 26))
+        p.map(brute, range(START, END))
 
 if __name__ == "__main__":
     max_thread = psutil.cpu_count()
